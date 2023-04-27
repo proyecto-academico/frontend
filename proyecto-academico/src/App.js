95bd7a7144e1f07f43503b5189c1f3870f8dcb11
Lucas-Porras <abdhalalucas@gmail.com>
2023-04-26 21:14:34 -0300

    Finalizamos el Login y reajustamos la imagen falta la API

--- proyecto-academico/src/App.js
+++ proyecto-academico/src/App.js
@@ -6,7 +6,6 @@ import FormLogin from './components/FormLogin';
 import './App.css';
 
 function App() {
-<<<<<<< HEAD
     return (
         <div className="App">
         
@@ -25,52 +24,6 @@ function App() {
                             </li>
                         </ul>
                     </div>
-=======
-  return (
-    <div className="App">
-      
-      <div class="espaciado">
-          <nav class="navbar border-bottom navbar-expand-lg">
-              <div class="container">
-                    <a class="navbar-brand" href="#">
-                    <img src="logo.png" alt="" class="col-sm-11" width="155px" height="155px" />
-                    </a>
-                  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                  </button>
-              <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                  <hr/>
-                  <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                      <li class="nav-item">
-                          <a class="nav-link text-center" href="#">Inicio</a>
-                      </li>
-                  </ul>
-              </div>
-            </div>
-          </nav>
-      </div>
-
-      <div class="container">
-          <div class="espaciado-def row justify-content-center">
-              <div class="espaciado-login p-4 rounded">
-                  <div class="">
-                      <h2 class="text-center">Iniciar sesión</h2>
-                      <div class="form rounded shadow p-3">
-                          <form method="POST" action="">
-                              <div class="form-group">
-                                  <div>
-                                      <label for="username"><b>Usuario</b></label>
-                                      <input type="text" class="form-control mt-1" placeholder="Ingrese su nombre de usuario" name="username" required/>
-                                  </div>
-                                  <div>
-                                      <label for="pwd" class="mt-1"><b>Contraseña</b></label>
-                                      <input type="password" class="form-control" placeholder="Ingrese su contraseña" name="pdw" required/>
-                                  </div>
-                                  <button type="submit" class="btn btn-success mt-3 w-100">Ingresar</button>
-                              </div>
-                          </form>
-                      </div>
->>>>>>> 5df8b1220b4703edcde523d616f43842b5a4a362
                   </div>
                 </nav>
             </div>
--- proyecto-academico/src/components/FormLogin.js
+++ proyecto-academico/src/components/FormLogin.js
@@ -7,51 +7,60 @@ const FormLogin = () => {
         pwd:''
     })
     const handleInputChange = (event) => {
-        console.log("funciona")
+        setDatos({
+            ...datos,
+            [event.target.name] : event.target.value
+        })
+    }
+    const enviarDatos = (event) => {
+        event.preventDefault(); // Cancela el direccionamiento a la API (una cosa así)
+        //console.log(datos.username + ' ' + datos.pwd)
+        fetch("", { // Falta la API
+            method: 'post',
+           body: datos,
+        })
     }
 
-    render(){
-        return(
-            <Fragment>
-                <div class="container">
-                    <div class="espaciado-def row justify-content-center">
-                        <div class="espaciado-login p-4 rounded">
-                            <div class="">
-                                <h2 class="text-center">Iniciar sesión</h2>
-                                <div class="form rounded shadow p-3">
-                                    <form method="POST" action="">
-                                        <div class="form-group">
-                                            <div>
-                                                <label for="username"><b>Usuario</b></label>
-                                                <input 
-                                                type="text" 
-                                                class="form-control mt-1" 
-                                                placeholder="Ingrese su nombre de usuario" 
-                                                name="username"
-                                                onChange={handleInputChange} 
-                                                required/>
-                                            </div>
-                                            <div>
-                                                <label for="pwd" class="mt-1"><b>Contraseña</b></label>
-                                                <input 
-                                                type="password" 
-                                                class="form-control" 
-                                                placeholder="Ingrese su contraseña" 
-                                                name="pwd"
-                                                onChange={handleInputChange} 
-                                                required/>
-                                            </div>
-                                            <button type="submit" value="submit" class="btn btn-success mt-3 w-100">Ingresar</button>
+    return(
+        <Fragment>
+            <div class="container">
+                <div class="espaciado-def row justify-content-center">
+                    <div class="espaciado-login p-4 rounded">
+                        <div class="">
+                            <h2 class="text-center">Iniciar sesión</h2>
+                            <div class="form rounded shadow p-3">
+                                <form method="POST" action="" onSubmit={enviarDatos}>
+                                    <div class="form-group">
+                                        <div>
+                                            <label for="username"><b>Usuario</b></label>
+                                            <input 
+                                            type="text" 
+                                            class="form-control mt-1" 
+                                            placeholder="Ingrese su nombre de usuario" 
+                                            name="username"
+                                            onChange={handleInputChange} 
+                                            required/>
                                         </div>
-                                    </form>
-                                </div>
+                                        <div>
+                                            <label for="pwd" class="mt-1"><b>Contraseña</b></label>
+                                            <input 
+                                            type="password" 
+                                            class="form-control" 
+                                            placeholder="Ingrese su contraseña" 
+                                            name="pwd"
+                                            onChange={handleInputChange} 
+                                            required/>
+                                        </div>
+                                        <button type="submit" value="submit" class="btn btn-success mt-3 w-100">Ingresar</button>
+                                    </div>
+                                </form>
                             </div>
                         </div>
                     </div>
                 </div>
-            </Fragment>
-        );
-    }
+            </div>
+        </Fragment>
+    );
 }
 
 
